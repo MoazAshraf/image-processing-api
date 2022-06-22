4e58244fc5265f741b2f741af92616fd722aedbc
Moaz Ashraf <mouadhashraf@gmail.com>
2022-06-22 02:49:33 +0200

    Install morgan & nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import logging from 'morgan';
 import routes from './routes/routes';
 
 const port = 8086;
 const host = 'localhost';
 const app = express();
 
+app.use(logging('dev'));
 app.use('/', routes);
 
-app.listen(port, host, () => {
+app.listen(port, host, (): void => {
     // eslint-disable-next-line no-console
     console.log(`Server is running at http://${host}:${port}`);
 });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (req, res) => {
+    res.status(200);
     res.send('Hello, world!');
 });
 
