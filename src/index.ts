b27ddba33f2e8f83ec3f1f6c2f9854981eea6781
Moaz Ashraf <mouadhashraf@gmail.com>
2022-06-22 02:42:28 +0200

    Basic express endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import routes from './routes/routes';
 
 const port = 8086;
 const host = 'localhost';
 const app = express();
 
+app.use('/', routes);
+
 app.listen(port, host, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server is running at http://${host}:${port}`);
 });
 
