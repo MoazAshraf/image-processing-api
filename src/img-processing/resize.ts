a1494be78da5d74a800a4db71e83a8fc44bcdf31
Moaz Ashraf <mouadhashraf@gmail.com>
2022-06-22 03:45:31 +0200

    Server resized images, caching

---
+++ src/img-processing/resize.ts
@@ -0,0 +1,21 @@
+import sharp from 'sharp';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+// Resize the image from inputPath and save it to outputPath, returns if the
+// outputPath already exists
+export default async (
+    inputPath: string,
+    outputPath: string,
+    width: number,
+    height: number
+) => {
+    try {
+        // File exists
+        await fs.stat(outputPath);
+    } catch (err) {
+        // File doesn't exist
+        await fs.mkdir(path.dirname(outputPath), { recursive: true });
+        await sharp(inputPath).resize(width, height).toFile(outputPath);
+    }
+};
