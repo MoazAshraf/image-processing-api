bd36ab31ce195a99ba4d8876c18e51f8b2a3ce8e
Moaz Ashraf <mouadhashraf@gmail.com>
2022-06-22 03:09:58 +0200

    Serve images using filename URL query param

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,26 @@
 import { Router } from 'express';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 const routes = Router();
 
 routes.get('/', (req, res) => {
-    res.status(200);
-    res.send('Hello, world!');
+    if ('filename' in req.query) {
+        const filepath = path.join(
+            __dirname,
+            '..',
+            'full',
+            req.query.filename as string
+        );
+        res.sendFile(filepath, (err) => {
+            res.send(`File "${filepath}" not found.`);
+            res.status(404);
+        });
+        res.status(200);
+    } else {
+        res.send('"filename" query param missing from URL.');
+        res.status(400);
+    }
 });
 
 export default routes;
